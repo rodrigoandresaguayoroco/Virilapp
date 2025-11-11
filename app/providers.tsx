25a824dc9a82af9c59e27c32345a550eccf16713
Rodrigo Aguayo <rodrigoandresaguayoroco@gmail.com>
2025-11-11 01:02:13 -0300

    Fix: Eliminada referencia a next-auth obsoleta

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,14 +1,15 @@
 'use client'
 
-import { SessionProvider } from 'next-auth/react'
 import { Inter } from 'next/font/google'
+import Cursor from '@/components/cursor'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <SessionProvider>
-      <div className={inter.className}>{children}</div>
-    </SessionProvider>
+    <div className={inter.className}>
+      <Cursor />
+      {children}
+    </div>
   )
 }
